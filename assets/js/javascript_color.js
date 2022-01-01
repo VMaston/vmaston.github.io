d2325722b53e73998e27f54232d0305df839682e
VincentMaston <vincentmaston@outlook.com>
2022-01-01 09:08:08 +0000

    integration of cookies to persist colors

--- assets/js/javascript_color.js
+++ assets/js/javascript_color.js
@@ -1,5 +1,12 @@
 let i = 0;
-const colors = ['yellowgreen', 'lightcoral', 'gold'];
+const colors = ['yellowgreen', 'lightcoral', 'gold', 'lightblue'];
+
+currentColor = getCookie('color');
+if (currentColor !== null) {
+  i = currentColor;
+  console.log(i);
+  setColor();
+}
 
 function colorPick() {
   if (i >= 0 && i < colors.length - 1) {
@@ -7,5 +14,11 @@ function colorPick() {
   } else {
     i = 0;
   }
+
+  setColor();
+}
+
+function setColor() {
   document.documentElement.style.setProperty('--accent', `${colors[i]}`);
+  setCookie('color', i);
 }
